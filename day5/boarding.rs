1d4279ed859794bbefb833af7aa630cc5247bbd2
Smilliam <smilliam@gmail.com>
2020-12-05 01:25:22 -0700

    Remove indexed vector, just track upper and lower bounds

--- day5/boarding.rs
+++ day5/boarding.rs
@@ -15,8 +15,6 @@ fn main() {
     let file = fs::File::open(filename).expect("Unable to open file");
     let reader = io::BufReader::new(file);
 
-    let search_space: Vec<i32> = make_search_vec(TOTAL_ROWS);
-
     let mut min_id: i32 = i32::MAX;
     let mut max_id: i32 = i32::MIN;
     let mut id_sum: i32 = 0;
@@ -24,8 +22,8 @@ fn main() {
     for line in reader.lines() {
         let text = line.unwrap();
 
-        let row: i32 = get_row(&search_space[0..TOTAL_ROWS], &text[0..BSP_ROW_SIZE]);
-        let col: i32 = get_col(&search_space[0..TOTAL_COLS], &text[BSP_ROW_SIZE..]);
+        let row: i32 = get_row(0, TOTAL_ROWS as i32 - 1, &text[0..BSP_ROW_SIZE]);
+        let col: i32 = get_col(0, TOTAL_COLS as i32 - 1, &text[BSP_ROW_SIZE..]);
         let id: i32 = get_seat_id(&row, &col);
         ids.push(id);
         id_sum += id;
@@ -37,50 +35,41 @@ fn main() {
     print_my_seat_id(&min_id, &max_id, &id_sum);
 }
 
-fn get_row<'a>(to_search: &'a [i32], row_str: &'a str) -> i32 {
+fn get_row<'a>(lower: i32, upper: i32, row_str: &'a str) -> i32 {
     let mut row_bool: Vec<bool> = Vec::new(); 
     for ch in row_str.chars() {
         row_bool.push(ch == 'B'); 
     }
 
-    return binary_partition(to_search, &row_bool[0], &row_bool[1..]);
+    return binary_partition(lower, upper, &row_bool[0], &row_bool[1..]);
 }
 
-fn get_col<'a>(to_search: &'a [i32], col_str: &'a str) -> i32 {
+fn get_col<'a>(lower: i32, upper: i32, col_str: &'a str) -> i32 {
     let mut col_bool: Vec<bool> = Vec::new();
     for ch in col_str.chars() {
         col_bool.push(ch == 'R');
     }
 
-    return binary_partition(to_search, &col_bool[0], &col_bool[1..]);
+    return binary_partition(lower, upper, &col_bool[0], &col_bool[1..]);
 }
 
 fn get_seat_id(row: &i32, col: &i32) -> i32 {
     return row * 8 + col;
 }
 
-fn binary_partition<'a>(haystack: &'a [i32], head: &'a bool, rest: &'a [bool]) -> i32 {
+fn binary_partition<'a>(lower: i32, upper: i32, head: &'a bool, rest: &'a [bool]) -> i32 {
     if rest.len() == 0 {
-        return haystack[if *head {1} else {0}];
+        return if *head {upper} else {lower};
     }
 
     // It feels like there should be a branchless way to do this...
-    let mid: usize = haystack.len() / 2usize;
+    let mid: i32 = (lower + upper + 1) / 2i32;
     if *head {
-        return binary_partition(&haystack[mid..], &rest[0], &rest[1..]);
+        return binary_partition(mid, upper, &rest[0], &rest[1..]);
     }
     else {
-        return binary_partition(&haystack[0..mid], &rest[0], &rest[1..]);
+        return binary_partition(lower, mid-1, &rest[0], &rest[1..]);
     }
-}
-
-fn make_search_vec(num_rows: usize) -> Vec<i32> {
-    let mut v: Vec<i32> = Vec::new();
-    for ii in 0..num_rows {
-        v.push(ii as i32);
-    }
-
-    return v;
 }
 
 fn sum_range(start: i32, end: i32) -> i32 {
